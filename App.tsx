67c07c4f501a0581feba86aa944d3bfb32f09fcc
Alice <alice@gmail.com>
2025-01-28 17:11:27 +0300

    git init

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Привет!</Text>
       <StatusBar style="auto" />
+      <TextInput placeholder="Введите текст" />
+      <Button title="Нажми меня"></Button>
     </View>
   );
 }
