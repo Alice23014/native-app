0f876af2d85a5c3bdbcec1690c58b485800a0e90
Alexander Shuvalov <alex-shuv@mail.ru>
2025-02-02 19:59:59 +0300

    add login screen

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Привет!</Text>
-      <StatusBar style="auto" />
-      <TextInput placeholder="Введите текст" />
-      <Button title="Нажми меня"></Button>
+      <View style={styles.content}>
+      <Text>PURPLESCHOOL</Text>
+      <View style={styles.form}>
+        <TextInput style={styles.input}></TextInput>
+        <TextInput style={styles.input}></TextInput>
+        <Pressable>
+          <Text>Войти</Text>
+        </Pressable>
+      </View>
+      <Text>Восстановить пароль</Text>
+      </View>
+      
     </View>
   );
 }
@@ -16,7 +24,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    padding: 55,
+  },
+  content:{
+    alignItems: 'center',
+    gap: 50,
+  },
+  form:{
+    alignSelf:'stretch',
+    gap: 16,
+  },
+  input:{
+    backgroundColor: '#2e2d3d'
   },
 });
